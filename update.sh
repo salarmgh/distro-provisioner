e42ae6117e8775bbd40fc97c1399d7b99f009c3a
Salar Moghaddam <salarmgh@gmail.com>
2021-11-03 14:42:55 +0330

    Update

--- update.sh
+++ update.sh
@@ -4,3 +4,7 @@ cp ~/.emacs $files/emacs
 cp ~/.gitconfig $files/gitconfig
 cp ~/.tmux.conf $files/tmux.conf
 cp ~/.vimrc $files/vimrc
+cp ~/.config/i3/config $files/i3config
+cp ~/.gitconfig $files/gitconfig
+cp ~/.gtkrc-2.0 $files/gtkrc-2.0
+cp ~/.config/gtk-3.0/gtkrc-3.0.ini $files/gtkrc-3.0.ini
